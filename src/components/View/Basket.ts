cb4e0c8055a32f0f3e9690ec8bf0cb9b7f8a937b
Arestov Aleksandr <dobro.world@yandex.ru>
2026-03-24 19:15:24 +0700

    Added ts file Basket

---
+++ src/components/View/Basket.ts
@@ -0,0 +1,33 @@
+import { ensureElement } from "../../utils/utils";
+import { Component } from "../base/Component";
+import { IEvents } from "../base/Events";
+
+ interface IBasket {
+    value: number;
+ }
+
+export class Basket extends Component<IBasket> {
+    basketList: HTMLElement;
+    basketTotalPrice: HTMLElement;
+    basketButton: HTMLButtonElement;
+
+    constructor(protected events: IEvents, container: HTMLElement) {
+        super(container);
+
+        this.basketList = ensureElement<HTMLElement>('.basket__list', this.container);
+        this.basketTotalPrice = ensureElement<HTMLElement>('.basket__price', this.container);
+        this.basketButton = ensureElement<HTMLButtonElement>('.basket__button', this.container);
+
+        this.basketButton.addEventListener('click', () => {
+            this.events.emit('order:start');
+        });
+    }
+
+    set listItems(items: HTMLElement[]) {
+        this.basketList.replaceChildren(...items);
+    }
+
+    set totalPrice(value: number) {
+        this.basketTotalPrice.textContent = `${value} синапсов`;
+    }
+}
